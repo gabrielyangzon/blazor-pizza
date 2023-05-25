a53700dc4e66fa191fe21bac3f34d6620734dcd7
gy04365249 <gy04365249@shi-g.com>
2023-05-25 09:04:42 +0900

    update changes

---
+++ BlazingPizza.Client/OrderState.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazingPizza.Client
+{
+    public class OrderState
+    {
+        public bool ShowingConfigureDialog { get; private set; }
+
+        public Pizza ConfiguringPizza { get; private set; }
+
+        public Order Order { get; private set; } = new Order();
+    }
+}
